9fb4383fde3c1359f63d105eb68976d0058e0238
IvanaMladenovicIT18 <mladenovic.it18.2020@uns.ac.rs>
2024-07-22 16:42:29 +0200

    User registration on the backend

---
+++ Backend/routes/userRoute.js
@@ -0,0 +1,9 @@
+
+import express from 'express';
+import userController from '../controllers/userController.js';
+
+const userRouter = express.Router();
+
+userRouter.post('/register', userController.register);
+
+export default userRouter;
